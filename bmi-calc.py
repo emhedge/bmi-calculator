f187e13b1e0dc0661b1ef8d32cfc9674334c12f2
emhedge <122066271+emhedge@users.noreply.github.com>
2023-10-03 15:22:44 +0800

    Created the formula for the BMI calc
    Defined height, weight, and BMI. This formula will serve as the
    backbone of my calculator. Next, I'll draft responses based on various
    input.

---
+++ bmi-calc.py
@@ -0,0 +1,10 @@
+name = input("Enter your name: ")
+
+weight = int(input("Enter your weight in kilograms: "))
+
+height = int(input("Enter your height in centimeters: "))
+
+
+BMI = ((weight / (height * height)) * 10000)
+
+print(BMI)
